6e0080f543533c006c82cc30555371afc90bdaaf
Yoshida <yoshida-1y-2o-3s-4h-5i-6d-7a@i.softbank.jp>
2018-11-27 15:23:10 +0900

    improve for ios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,8 @@
     <link rel="manifest" href="/manifest.json">
     <link rel="apple-touch-icon" href="/launcher-icon-4x.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="apple-mobile-web-app-title" content="散歩Snap">
 </head>
 <body>
     <div id="app">
