29c12c400af58ab084a12b89c9e0316278698ba0
Yoshida <yoshida-1y-2o-3s-4h-5i-6d-7a@i.softbank.jp>
2019-02-25 05:25:09 +0900

    support db slave

--- config/database.php
+++ config/database.php
@@ -41,8 +41,14 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'read' => [
+                'host' => env('DB_SLAVE_HOST', '127.0.0.1'),
+                'port' => env('DB_SLAVE_PORT', '3306'),
+            ],
+            'write' => [
+                'host' => env('DB_HOST', '127.0.0.1'),
+                'port' => env('DB_PORT', '3306'),
+            ],
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
